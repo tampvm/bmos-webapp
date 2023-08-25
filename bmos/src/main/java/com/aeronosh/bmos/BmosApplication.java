c0298b7695a8c15a48dd2ee1a1adde992dd7c333
tampvm <minhtam250102@gmail.com>
2023-08-25 23:18:41 +0700

    first commit

---
+++ bmos/src/main/java/com/aeronosh/bmos/BmosApplication.java
@@ -0,0 +1,13 @@
+package com.aeronosh.bmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmosApplication.class, args);
+	}
+
+}
---
+++ bmos/src/test/java/com/aeronosh/bmos/BmosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeronosh.bmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
