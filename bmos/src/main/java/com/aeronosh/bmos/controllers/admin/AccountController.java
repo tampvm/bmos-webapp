3a5214626483368c51141364efe2085d826ee6ef
thanhlong109 <thinghiemminiworld@gmail.com>
2023-09-04 20:37:27 +0700

    add form add user, update layout component,add data rest

--- bmos/src/main/java/com/aeronosh/bmos/controllers/admin/AccountController.java
+++ bmos/src/main/java/com/aeronosh/bmos/controllers/admin/AccountController.java
@@ -1,15 +1,23 @@
 package com.aeronosh.bmos.controllers.admin;
 
+import com.aeronosh.bmos.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/admin/tai-khoan")
 public class AccountController {
 
-    @RequestMapping("/tai-khoan")
+    @RequestMapping("")
     public String showManageAccount(){
         return "admin/manage-account";
+    }
+
+    @RequestMapping("/them-moi")
+    public String showAddForm(Model model){
+        model.addAttribute(new User());
+        return "admin/add-account";
     }
 
 }
